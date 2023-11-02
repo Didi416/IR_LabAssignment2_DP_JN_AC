ebc08d9d73af7a6495d85e9da194a4d9456c2550
Dyandra Prins <114969178+Didi416@users.noreply.github.com>
2023-11-02 21:59:43 +1100

    Update Lab2_Assignment.m

--- Lab2_Assignment.m
+++ Lab2_Assignment.m
@@ -1,115 +1,213 @@
 classdef Lab2_Assignment < handle
-    properties
-        r
+    properties (Access = private)
+        lab2GuiApp  % Instance of Lab2_GUI
     end
-   methods
-       function self = Lab2_Assignment()
-           clf
-           clc
-           Environment_Lab2();
-           self.clawAttach(-1,0,0);
-       end
-   end
 
-   %% Defined positions of Groceries
-   methods
-       function self = clawAttach(self, X, Y, Z)
-           baseTr = transl(X, Y, Z);
-           self.r = LinearUR10(baseTr);
-           hold on;
-           Claw1 = TheClaww();
-           Claw2 = TheClaww();
+    properties (Constant)
+        estopStatus = 'Run'; % Initialize to 'Run'
+    end
 
-           q = [0,0,0,0,0,0,0];
-           Claw1.model.base = self.r.model.fkine(q).T * transl(0,0,0.09);
-           Claw1.model.animate(q);
-           drawnow;
-           Claw2.model.base = self.r.model.fkine(q).T * trotz(pi) * transl(0,0,0.09);
-           Claw2.model.animate(q);
-           drawnow;
-           input("Press ENTER to finish.")
-       end
-   end
-   methods (Static)
-       function objectLocater()
-          self.r = LinearUR10;
-          q = [0, 0, 0, 0, 0, 0, 0];
+    properties
+        robot1 % Instance of the first robot
+        robot2 % Instance of the second robot
+        % Instances of grippers
+        Claw1_1
+        Claw1_2
+        Claw2_1
+        Claw2_2
+    end
 
-          endPos = [-0.9, 0.4, 0.50;
-                    -0.9, 0.4, 0.55
-                     0.9, 0.4, 0.60;
-                    -0.6, 0.4, 0.50;
-                    -0.6, 0.4, 0.55];
+    %% Non-Static Methods
+    methods
+        %% Constructor
+        function self = Lab2_Assignment()
+            clc
+            clf
+            % Create an instance of Lab2_GUI
+            % self.lab2GuiApp = Lab2_GUI();
+            Environment_Lab2();
+            self.initialiseRobots(-0.8,0,0);
+            input("Press ENTER to start.");
+            self.main();
+        end
+        %% Start GUI App and Enable eStop functionalities
+        function startApp(self)
+            % Start the Lab2_GUI app
+            self.lab2GuiApp.UIFigure.Visible = 'on';
+        end
+        function toggleEstop(estopStatus)
+            % Toggle the E-stop status between 'Run' and 'Stop'
+            if strcmp(estopStatus, 'Run')
+                estopStatus = 'Run';
+            else
+                estopStatus = 'Stop';
+            end
+        end
+        %% Initialise Robots in Environment
+        function initialiseRobots(self, X, Y, Z)
+            % Initialise robot instances
+            baseTr = transl(X, Y, Z);
+            self.robot1 = LinearUR10(baseTr);
+            self.robot2 = Robo();
+            startQ1 = deg2rad([0,90,45,120,45,-90,0]);
+            self.robot1.model.animate(startQ1);
+            startQ2 = deg2rad([0,-65,100,-180,-90,0]);
+            self.robot2.model.animate(startQ2)
+            % self.clawAttach();
+        end
+        %% Attach Gripper to Robot End Effectors
+        function clawAttach(self)
+            self.Claw1_1 = TheClaww();
+            self.Claw1_2 = TheClaww();
+            self.Claw2_1 = TheClaww();
+            self.Claw2_2 = TheClaww();
 
-           pickUps = [-0.9, -0.4, 0.7;
-                      -0.6, -0.4, 0.7
-                      -0.3, -0.4, 0.7;
-                      -0.1, -0.4, 0.7;
-                      -1.2, -0.2, 0.7];
+            endQ1 = self.robot1.model.getpos();
+            endQ2 = self.robot2.model.getpos();
+            q = [pi/2,0,-pi/4];
+            % Linear UR10 Gripper
+            self.Claw1_1.model.base = self.robot1.model.fkine(endQ1).T * transl(0,0,0.09);
+            self.Claw1_1.model.animate(q);
+            drawnow;
+            self.Claw1_2.model.base = self.robot1.model.fkine(endQ1).T * trotz(pi) * transl(0,0,0.09);
+            self.Claw1_2.model.animate(q);
+            drawnow;
+            % ShelfElf Gripper
+            self.Claw2_1.model.base = self.robot2.model.fkine(endQ2).T;
+            self.Claw2_1.model.animate(q);
+            drawnow;
+            self.Claw2_2.model.base = self.robot2.model.fkine(endQ2).T * trotz(pi);
+            self.Claw2_2.model.animate(q);
+            drawnow;
+        end
 
-           for i = 1:size(endPos, 1)
-               endPosition = endPos(i, :);
-               hold = pickUps(i, :);
-               Lab2_Assignment.robotRotato(endPosition, hold, self.r);
-           end
-           input('Press enter to end operation');
-       end
+        %% Main Function
+        function main(self)
+            % inital placements of boxes
+            boxPos = zeros(12,3);
+            boxes = cell(1,12);
+            verticesArray = zeros(24,3,6);
+            x = -1.4;
+            i = 1;
 
-       function robotRotato(endPosition, hold, r)
-          numSteps = 100;
+            for y = -0.2:0.6:1.0
+                for z = 0.2:0.28:0.48
+                    boxes{i} = PlaceObject('box.ply');
+                    vertices = get(boxes{i}, 'Vertices');
+                    transformedVertices = [vertices,ones(size(vertices,1),1)] * transl(x,y,z)';
+                    set(boxes{i}, 'Vertices', transformedVertices(:,1:3));
+                    verticesArray(:,:,i) = vertices;
+                    boxPos(i,1) = x;
+                    boxPos(i,2) = y;
+                    boxPos(i,3) = z;
+                    i = i+1;
+                end
+                for z = 1.0:0.28:1.28
+                    boxes{i} = PlaceObject('box.ply');
+                    vertices = get(boxes{i}, 'Vertices');
+                    transformedVertices = [vertices,ones(size(vertices,1),1)] * transl(x,y,z)';
+                    set(boxes{i}, 'Vertices', transformedVertices(:,1:3));
+                    verticesArray(:,:,i) = vertices;
+                    boxPos(i,1) = x;
+                    boxPos(i,2) = y;
+                    boxPos(i,3) = z;
+                    i = i+1;
+                end
+            end
 
-          endTransform = transl(hold) * trotx(pi);
-          elbowAngles = deg2rad([0, 0, 45, 70, -35, 259, 0]);
-          qEnd = self.r.model.ikcon(endTransform, elbowAngles);
+            elbowUp1 = deg2rad([0,90,45,120,45,-90,0]);
+            elbowUp2 = deg2rad([0,-65,100,-180,-90,0]);
 
-          for numBricks = 1:1
-              qCurrent = self.r.model.getpos();
-              plotTraj = jtraj(qCurrent, qEnd, numSteps);
+            for i = 1:2
+                numSteps = 100;
+                x = boxPos(i,1) + 0.18;
+                y = boxPos(i,2);
+                z = boxPos(i,3) + 0.035;
 
-              for i = 1:min(size(plotTraj, 1), numSteps)
-                  self.r.model.animate(plotTraj(i, :));
+                % Calculate start and end pose for the RMRC
+                currentPos = self.robot1.model.getpos();
+                pickUpBox = transl(x, y, z) * troty(-pi/2);
+                pickUpBoxEndPose = self.robot1.model.ikcon(pickUpBox, elbowUp1);
+                robot1Trajectory = jtraj(currentPos, pickUpBoxEndPose, numSteps);
+                for x = 1:numSteps
+                   self.robot1.model.animate(robot1Trajectory(x,:));
+                   drawnow();
+                end
+                disp("Robot arm positioned.");
 
-                  %% Claw
-                    
-                  endEffectorTransform = self.r.model.fkine(plotTraj(i, :)).T;
-                  Claw1.model.base = endEffectorTransform;
-                  %Claw1.model.animate([0, 0]);
-                  position = plotTraj(i,:);
-                  endEffector = self.r.model.fkine(position).T;
-                  Claw1.model.base = endEffector * trotx(pi/2);
-                  %Claw1.model.animate([0, 0]);
-                  pause(0.01);
-              end
+                % Call the RMRC function instead of using ikcon
+                % self.RMRC(currentPos, pickUpBoxEndPose, numSteps, elbowUp1); % Example: 100 steps
+            end
+            
+            % input('Press enter to end operation');
+        end
 
-              qCurrent = self.r.model.getpos();
-              endTransform = transl(endPosition) * trotx(pi);
-              elbowEndAngles = deg2rad([0, 0, 45, 70, -35, 259, 0]);
-              BrickEnd = self.r.model.ikcon(endTransform, elbowEndAngles);
-              plotTrajEnd = jtraj(qCurrent, BrickEnd, numSteps);
+        function RMRC(self, currentPos, endPose, numSteps, elbowUp)
+            t = 10;             % Total time (s)
+            deltaT = 0.02;      % Control frequency
+            steps = t/deltaT;   % No. of steps for simulation
+            epsilon = 0.5;      % Threshold value for manipulability/Damped Least Squares
+            W = diag([1 1 1 0.1 0.1 0.1]);    % Weighting matrix for the velocity vector
 
-              for i = 1:numSteps
-                  currentTransform = self.r.model.fkine(plotTrajEnd(i, :)).T;
-                  self.r.model.animate(plotTrajEnd(i, :));
-                  pause(0.01);
-              end
-          end
-       end
-   end
+            m = zeros(steps,1);             % Array for Measure of Manipulability
+            qMatrix = zeros(steps,7);       % Array for joint anglesR
+            qdot = zeros(steps,7);          % Array for joint velocities
+            theta = zeros(3,steps);         % Array for roll-pitch-yaw angles
+            x = zeros(3,steps);             % Array for x-y-z trajectory
 
-       %{
+            robot1Trajectory = jtraj(currentPos, endPose, numSteps);
+            % s = lspb(0,1,steps);                                                        % Trapezoidal trajectory scalar
+            for i = 1:numSteps
+                T = self.robot1.model.fkine(robot1Trajectory(i,:)).T;
+                x(1,i) = T(1,4);                                                        % Points in x
+                x(2,i) = T(2,4);                                                        % Points in y
+                x(3,i) = T(3,4);                                                        % Points in z
+                theta(1,i) = atan(T(2,1)/T(1,1));                                       % Roll angle
+                theta(2,i) = atan(-T(3,1)/sqrt((T(3,2)^2)+(T(3,3)^2)));                 % Pitch angle
+                theta(3,i) = atan(T(3,2)/T(3,3));                                       % Yaw angle
+            end
+
+            T = [rpy2r(theta(1,1),theta(2,1),theta(3,1)) x(:,1);zeros(1,3) 1];          % Create transformation of first point and angle
+            qMatrix(1,:) = self.robot1.model.ikcon(T,elbowUp);                         % Solve joint angles to achieve first waypoint
+
+            for i = 1:steps-1
+                T = self.robot1.model.fkine(qMatrix(i,:)).T;                            % Get forward transformation at current joint state
+                deltaX = x(:,i+1) - T(1:3,4);                                         	% Get position error from next waypoint
+                Rd = rpy2r(theta(1,i+1),theta(2,i+1),theta(3,i+1));                     % Get next RPY angles, convert to rotation matrix
+                Ra = T(1:3,1:3);                                                        % Current end-effector rotation matrix
+                Rdot = (1/deltaT)*(Rd - Ra);                                            % Calculate rotation matrix error
+                S = Rdot*Ra';                                                           % Skew symmetric!
+                linear_velocity = (1/deltaT)*deltaX;
+                angular_velocity = [S(3,2);S(1,3);S(2,1)];                              % Check the structure of Skew Symmetric matrix!!
+                % deltaTheta = tr2rpy(Rd*Ra');                                          % Convert rotation matrix to RPY angles
+                xdot = W*[linear_velocity;angular_velocity];                          	% Calculate end-effector velocity to reach next waypoint.
+                J = self.robot1.model.jacob0(qMatrix(i,:));                             % Get Jacobian at current joint state
+                m(i) = sqrt(det(J*J'));
+                if m(i) < epsilon  % If manipulability is less than given threshold
+                    lambda = (1 - m(i)/epsilon)*5E-2;
+                else
+                    lambda = 0;
+                end
+                invJ = pinv(J) + lambda * eye(7);                                       % DLS Inverse
+                qdot(i,:) = (invJ*xdot)';                                               % Solve the RMRC equation (you may need to transpose the         vector)
+                for j = 1:6                                                             % Loop through joints 1 to 6
+                    if qMatrix(i,j) + deltaT*qdot(i,j) < self.robot1.model.qlim(j,1)                 % If next joint angle is lower than joint limit...
+                        qdot(i,j) = 0; % Stop the motor
+                    elseif qMatrix(i,j) + deltaT*qdot(i,j) > self.robot1.model.qlim(j,2)             % If next joint angle is greater than joint limit ...
+                        qdot(i,j) = 0; % Stop the motor
+                    end
+                end
+                qMatrix(i+1,:) = qMatrix(i,:) + deltaT*qdot(i,:);                       % Update next joint state based on joint velocities
+            end
+        end
+    end
+
+    %{
        function collisionDetected = IsCollisionWithEnvironment(currentTransform, environmentObjects)
            % Check for collisions between robot and environment objects
            % Implement your collision detection logic here
            collisionDetected = false; % Update this based on collision checking
-       end
+       end        
+    %}
 
-       function environmentObjects = loadEnvironmentObjects()
-        % Define or load information about environment objects in this function
-        % This can include defining object shapes and positions
-        % For example, you can create a cell array or a struct to represent the objects
-        % Example: environmentObjects = {object1, object2, object3, ...};
-        end
-        
-       %}
-   
 end
